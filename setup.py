d51dacbc8b8da00e1bf330a88145cfbe1a1f2693
Y00N <xodud9752@gmail.com>
2022-05-04 09:23:06 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='mycalculator0504',
+  version='1.0.0',
+  description='my_calc_YTY',
+  author='YTY',
+  author_email='xodud9752@gmail.com',
+  url='https://github.com/TAEYOUNGY00N/Calc0504/',
+  install_requires=['pytest'],
+  long_description='my_calc YTY',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python ::3",
+    "Operating System :: OS Independent",
+  ],
+)
