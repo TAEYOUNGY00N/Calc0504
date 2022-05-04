e94f95e0ff8a18468469140cab23deaa743c260b
Y00N <xodud9752@gmail.com>
2022-05-04 09:51:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
   long_description='my_calc YTY',
   long_description_content_type='text/markdown',
   classifiers=[
-    "Programming Language :: Python :: 3",
+    "Programmming Language :: Python :: 3",
     "Operating System :: OS Independent",
   ],
 )
