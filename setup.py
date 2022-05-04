28cb18de55dadce1019de3c7b2f8192930d19537
Y00N <xodud9752@gmail.com>
2022-05-04 09:31:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   author='YTY',
   author_email='xodud9752@gmail.com',
   url='https://github.com/TAEYOUNGY00N/Calc0504/',
-  download_url=' ',
+  download_url='https://github.com/TAEYOUNGY00N/Calc0504/archive/refs/tags/v1.0.0.zip',
   install_requires=['pytest'],
   long_description='my_calc YTY',
   long_description_content_type='text/markdown',
